a8f88ca8eaa00f4e5794c5ab0ce167a825d055db
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-06-23 14:00:03 -0400

    re-add setupy.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+import versioneer
+
+setup(
+    name="lmoments3",
+    packages=["lmoments3"],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
